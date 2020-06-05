5459f37c285d6330de41c9d789078a6d2fd661b7
Lisa Wang <rwq987654321000@gmail.com>
2020-06-05 10:07:38 +0800

    Mail.h&keyword.h_finish

--- keyword.h
+++ keyword.h
@@ -1,15 +1,24 @@
 class Keyword{
     unordered_set<String<25>> data;
     vector <char> stack;
+    vector <int> num;
 public:
     bool match(Expression &exp) {
         for(int i = 0 ; i < exp.postfix.size() ; ++ i ){
             if(data.count(exp.postfix[i])) exp.postfix[i]=1;
-            else exp.postfix=0; // 會不小心把運算元判掉，我要所有運算元的set 之類ㄉ判斷
+            else if (exp.postfix[i] != "|" && exp.postfix[i] != "&" && exp.postfix[i] != "!") exp.postfix[i]=0; 
         }
         for(int i = 0 ; i < exp.postfix.size() ; ++ i ){
-            
+            if(exp.postfix[i] != "|" && exp.postfix[i] != "&" && exp.postfix[i] != "!") num.push_back(exp.postfix[i][0]-'0');
+            else if(exp.postfix[i] != "!") num.back() = !num.back() ;
+            else{
+                int second = num.back() ;
+                num.pop_back();
+                if(exp.postfix[i] != "&") num.back() = num.back() & second ;
+                else num.back() = num.back() | second ;
+            }
         }
+        return num[0] ;
     }
     void insert(String &s) {
         data.insert(s);
