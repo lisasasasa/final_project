300623aa270e42ef74a7ec47098ee8094c5fcd9b
baluteshih <brian369999@gmail.com>
2020-06-06 11:28:58 +0800

    remove vector in Keyword.h

--- Keyword.h
+++ Keyword.h
@@ -2,34 +2,31 @@ class Keyword{
     unordered_set<String<25>> data;
 public:
     bool match(Expression &exp) {
-        vector<char> stack;
-        vector<int> num;
+        int num_top = -1;
         //for (auto i:exp.postfix)
         //    i.print(),printf(" ");
        // puts("");
-        for(int i = 0, second; i < int(exp.postfix.size()) ; ++i ) {
+        for(int i = 0; i < int(exp.postfix.size()) ; ++i ) {
             switch (exp.postfix[i][0]) {
                 case '|':
-                    second = num.back();
-                    num.pop_back();
-                    num.back() = num.back() | second;
+                    temp_stack[num_top - 1] |= temp_stack[num_top];
+                    --num_top;
                     break;
                 case '&':
-                    second = num.back();
-                    num.pop_back();
-                    num.back() = num.back() & second;
+                    temp_stack[num_top - 1] &= temp_stack[num_top];
+                    --num_top;
                     break;
                 case '!':
-                    num.back() = !num.back();
+                    temp_stack[num_top] = !temp_stack[num_top];
                     break;
                 default:
                     //exp.postfix[i].print();
                     //printf(" %d\n",int(data.count(exp.postfix[i])));
-                    num.push_back(data.count(exp.postfix[i]));
+                    temp_stack[++num_top] = data.count(exp.postfix[i]);
                     break;
             }
         }
-        return num[0];
+        return temp_stack[0];
     }
     void insert(String<25> &s) {
         data.insert(s);
--- Parameter.h
+++ Parameter.h
@@ -1,7 +1,9 @@
-const int TEMP_SIZE = 10000000;
+const int TEMP_SIZE = 1000000;
+const int STACK_SIZE = 100;
 const int MAX_ID = 10000;
 const int user_range = 65536;
 char temp_string[TEMP_SIZE];
+int temp_stack[STACK_SIZE];
 
 char user[user_range][55];
 unsigned int user_hash(char *s, int flag = 0) { // Perfect hash
@@ -10,15 +12,17 @@ unsigned int user_hash(char *s, int flag = 0) { // Perfect hash
     for (char c, *p = s; (c = *p); ++p)
         if (c != '"')
             hash = (((hash << 6) + hash) + c) & (user_range - 1);
-    if (flag)
-        strcpy(user[hash], s);
+    if (flag) {
+        if (!user[hash][0])
+            strcpy(user[hash], s);
+    }
     else if (strcmp(user[hash], s))
         hash = 0;
     return hash;
 }
 
 bool isalphnum(char c) {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
 }
 
 
