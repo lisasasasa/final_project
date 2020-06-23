4222dffab31fac4476b3da79f9148d99f8f8b3c4
Lisa Wang <rwq987654321000@gmail.com>
2020-06-23 19:14:51 +0800

    make heap

--- Heap.h
+++ Heap.h
@@ -1,26 +1,58 @@
 template< size_t N, typename T >
 class Heap {
-    int cnt[N] ;
-    int Top = 0 ;
+    bitset<N> del;
+    int Length = 0;
     pair<T, int> data[N];
+    pair<T, int> tmp;
+    priority_queue < pair <T , int > > pq;
+    void pop_heap(int idx) {
+        data[idx] = data[Length];
+        --Length;
+        while( 2*idx >= Length ){
+            if( 2*idx  == Length ) data[idx] = tmp , tmp = data[2 * idx] , data[2 * idx] = data[idx] ;
+            else{
+                if( data[2*idx].first > data[ 2*idx + 1 ].first ) data[idx] = tmp , tmp = data[2 * idx] , data[2 * idx] = data[idx] ;
+                else data[idx] = tmp , tmp = data[2 * idx + 1 ] , data[2 * idx + 1 ] = data[idx] ;
+            }
+        }
+    }
 public:
     void push(T x,int num) {
-        data.push(pair<T, int>(x, -num));
+        del[num] = 0;
+        ++Length;
+        data[Length] = make_pair( x , num );
+        //data.push(pair<T, int>(x, -num));
     }
     T top() {
-        while (!data.empty() && cnt[-data.top().second])
-            --cnt[-data.top().second], data.pop();
-        return -data.top().second;
+        while ( Length > 0 && del[-data[1].second])
+            pop_heap(1);
+        return -data[1].second;
     }
     void erase(int num) {
-        ++cnt[num];
+        del[num] = 1;
     }
     bool empty() {
-        while (!data.empty() && cnt[-data.top().second])
-            --cnt[-data.top().second], data.pop();
-        return data.empty();
+        while ( Length > 0 && del[-data[1].second])
+            pop_heap(1);
+        return Length == 0 ;
     }
-    void kth() {
-
+    vector<int> kth(int k) {
+        vector <int> v ;
+        priority_queue < pair <T , int > > pq;
+        pq.push(make_pair(data[1].first, 1)); 
+        pair<T, int> top ;
+        for ( int i = 0 ; i < k ; ++i ){
+            top = pq.top(); 
+            pq.pop();
+            if (del[-data[top.second].second]) {
+                pop_heap(top.second);
+                --i;
+                continue;
+            }
+            v.push_back(-data[top.second].second);
+            pq.push(make_pair(data[ 2 * top.second()) ].first, 2 * top.second); 
+            pq.push(make_pair(data[ 2 * top.second()) + 1 ].first, 2 * top.second + 1); 
+        }
+        return v ;
     }
 };
